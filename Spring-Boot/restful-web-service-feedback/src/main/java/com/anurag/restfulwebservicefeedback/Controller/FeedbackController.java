165bb9ef8a0656ba7c97adc12ddd03fd1ef4f3d8
AnuragDhiman400 <anuragdhiman400@gmail.com>
2020-06-23 03:26:38 -0400

    New methods added!!

--- Spring-Boot/restful-web-service-feedback/src/main/java/com/anurag/restfulwebservicefeedback/Controller/FeedbackController.java
+++ Spring-Boot/restful-web-service-feedback/src/main/java/com/anurag/restfulwebservicefeedback/Controller/FeedbackController.java
@@ -1,8 +1,11 @@
 package com.anurag.restfulwebservicefeedback.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,10 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anurag.restfulwebservicefeedback.Bean.Review;
+import com.anurag.restfulwebservicefeedback.Bean.User;
 import com.anurag.restfulwebservicefeedback.Repository.ReviewRepository;
+import com.anurag.restfulwebservicefeedback.Repository.UserRepository;
 
 @RestController
 public class FeedbackController {
@@ -21,11 +27,21 @@ public class FeedbackController {
 	@Autowired
 	ReviewRepository reviewRepository;
 	
+	@Autowired
+	UserRepository userRepo;
+	
 	@GetMapping("/user/{section}/feedback")
 	public List<Review> getAllFeedback(@PathVariable String section)
 	{
 		
 		return reviewRepository.findBySection(section);
+	}
+	
+	@GetMapping("/jpa/user/feedback/username/{section}/{category}")
+	public @ResponseBody List<String> getAllusernames(@PathVariable String section, @PathVariable String category)
+	{
+	
+		return reviewRepository.findAllUsernames(section,category);
 	}
 	
 	@PutMapping("/user/{section}/feedback/{id}")
