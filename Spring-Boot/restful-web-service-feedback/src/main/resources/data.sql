aeb1d17c8c6517736ad351526e6c062521a9fa88
AnuragDhiman400 <anuragdhiman400@gmail.com>
2020-06-26 01:13:21 -0400

    database updated!!

--- Spring-Boot/restful-web-service-feedback/src/main/resources/data.sql
+++ Spring-Boot/restful-web-service-feedback/src/main/resources/data.sql
@@ -31,17 +31,17 @@ values(10009,'anurag8', 'anuragdhiman@gmail', 'd', 'student', 'A');
 
 
 --Insert into Review table
-insert into REVIEW(id,user_id, review,section,question1,question2,question3,question4,question5)
-values(1, 5, 'You are smart', 'A',1,'','','','');
+insert into REVIEW(id,username,question1,question2,question3,question4,question5)
+values(1, 'anurag1',1,2,1,5,4);
 
-insert into REVIEW(id,user_id, review,section,question1,question2,question3,question4,question5)
-values(2, 6, 'You are awesome','B','',1,'','','');
+insert into REVIEW(id, username,question1,question2,question3,question4,question5)
+values(2, 'anurag2',2,3,1,4,2);
 
-insert into REVIEW(id,user_id, review,section,question1,question2,question3,question4,question5)
-values(3, 7, 'You are great','C','','',1,'','');
+insert into REVIEW(id,username,question1,question2,question3,question4,question5)
+values(3, 'anurag3',3,2,2,2,2);
 
-insert into REVIEW(id,user_id, review,section,question1,question2,question3,question4,question5)
-values(4, 8, 'You are good','D','','','',1,'');
+insert into REVIEW(id, username,question1,question2,question3,question4,question5)
+values(4, 'anurag4',1,3,5,1,2);
 
-insert into REVIEW(id,user_id, review,section,question1,question2,question3,question4,question5)
-values(5, 9, 'You are cool','A','','','','',1);
+insert into REVIEW(id,username,question1,question2,question3,question4,question5)
+values(5, 'anurag5',5,4,3,2,1);
