67880a64898ebcaa0e800ae8878ee92d02155d66
AnuragDhiman400 <anuragdhiman400@gmail.com>
2020-07-04 15:49:58 -0400

    RatingRepo updated!!

--- Spring-Boot/restful-web-service-feedback/src/main/java/com/anurag/restfulwebservicefeedback/Repository/RatingRepo.java
+++ Spring-Boot/restful-web-service-feedback/src/main/java/com/anurag/restfulwebservicefeedback/Repository/RatingRepo.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.anurag.restfulwebservicefeedback.Bean.UserRating;
 
+//RatingRepo repository
 public interface RatingRepo extends JpaRepository<UserRating, Long> {
 
 }
