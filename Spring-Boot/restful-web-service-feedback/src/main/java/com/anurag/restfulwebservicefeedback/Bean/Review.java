21a267aeeb3b3353212d1cb2d185662c22007897
AnuragDhiman400 <anuragdhiman400@gmail.com>
2020-07-10 15:02:27 -0400

    Review model added

--- Spring-Boot/restful-web-service-feedback/src/main/java/com/anurag/restfulwebservicefeedback/Bean/Review.java
+++ Spring-Boot/restful-web-service-feedback/src/main/java/com/anurag/restfulwebservicefeedback/Bean/Review.java
@@ -9,32 +9,37 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Table(name="review")
 @Entity
 public class Review {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-
-	private int user_id;
-	private String review;
-	private String ratings;
-
-	private String section;
+	
+	private String username;
+	private int question1;
+	private int question2;
+	private int question3;
+	private int question4;
+	private int question5;
+	
 
 	protected Review() {
 	}
 
-
 	
-	public Review(Long id, int user_id, String review, String ratings, String section) {
+	public Review(Long id,String username, int question1, int question2, int question3, int question4, int question5) {
 		super();
 		this.id = id;
-		this.user_id = user_id;
-		this.review = review;
-		this.ratings = ratings;
-		this.section = section;
+		this.username = username;
+		this.question1 = question1;
+		this.question2 = question2;
+		this.question3 = question3;
+		this.question4 = question4;
+		this.question5 = question5;
 	}
 
 	public Long getId() {
@@ -45,37 +50,127 @@ public class Review {
 		this.id = id;
 	}
 
-	public int getUser_id() {
-		return user_id;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
+
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	public String getReview() {
-		return review;
+
+
+	public int getQuestion1() {
+		return question1;
 	}
 
-	public void setReview(String review) {
-		this.review = review;
+
+
+	public void setQuestion1(int question1) {
+		this.question1 = question1;
 	}
 
-	public String getRatings() {
-		return ratings;
+
+
+	public int getQuestion2() {
+		return question2;
 	}
 
-	public void setRatings(String ratings) {
-		this.ratings = ratings;
+
+
+	public void setQuestion2(int question2) {
+		this.question2 = question2;
 	}
 
-	public String getSection() {
-		return section;
+
+
+	public int getQuestion3() {
+		return question3;
 	}
 
-	public void setSection(String section) {
-		this.section = section;
+
+
+	public void setQuestion3(int question3) {
+		this.question3 = question3;
 	}
+
+
+
+	public int getQuestion4() {
+		return question4;
+	}
+
+
+
+	public void setQuestion4(int question4) {
+		this.question4 = question4;
+	}
+
+
+
+	public int getQuestion5() {
+		return question5;
+	}
+
+
+
+	public void setQuestion5(int question5) {
+		this.question5 = question5;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + question1;
+		result = prime * result + question2;
+		result = prime * result + question3;
+		result = prime * result + question4;
+		result = prime * result + question5;
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Review other = (Review) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (question1 != other.question1)
+			return false;
+		if (question2 != other.question2)
+			return false;
+		if (question3 != other.question3)
+			return false;
+		if (question4 != other.question4)
+			return false;
+		if (question5 != other.question5)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+
+
+
 
 
 }
