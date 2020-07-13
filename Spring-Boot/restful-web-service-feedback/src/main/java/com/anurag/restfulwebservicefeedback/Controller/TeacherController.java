4ff5245dfd5a4bf2f302b2195d429fb631209b0b
AnuragDhiman400 <anuragdhiman400@gmail.com>
2020-07-12 22:47:29 -0400

    teacherController update!!

---
+++ Spring-Boot/restful-web-service-feedback/src/main/java/com/anurag/restfulwebservicefeedback/Controller/TeacherController.java
@@ -0,0 +1,29 @@
+package com.anurag.restfulwebservicefeedback.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anurag.restfulwebservicefeedback.Bean.User;
+import com.anurag.restfulwebservicefeedback.Repository.UserRepository;
+
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
+public class TeacherController {
+	
+	@Autowired
+	UserRepository userRepo;
+	
+	
+	@GetMapping("/jpa/user/studentlist/{section}/{category}")
+	public List<User> getStudentList(@PathVariable String section, @PathVariable String category)
+	{
+		
+		return userRepo.findBySectionForTeacher(section, category);
+	}
+
+}
